0b69a67b3f59b7e5a8db72b62e57fae40b912804
Valentin <cristian.valentin.ghita06@gmail.com>
2026-08-14 21:25:41 +0200

    small updating, visual bugs Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -165,7 +165,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         {
             // Movimento pieno bloccato -> prova solo componente X
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if (canMove)
             {
@@ -175,7 +175,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
             {
                 // Anche X bloccato -> prova solo componente Z
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {
