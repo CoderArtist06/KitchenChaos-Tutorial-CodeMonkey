46b5d8b25a55f37b92d24359a1d14fb37cfd5296
Valentin <cristian.valentin.ghita06@gmail.com>
2026-08-21 17:18:44 +0200

    Plate World UI Icons

--- Assets/Scripts/Counters/PlateKitchenObject.cs
+++ Assets/Scripts/Counters/PlateKitchenObject.cs
@@ -38,8 +38,13 @@ public class PlateKitchenObject : KitchenObject
             {
                 kitchenObjectSO = kitchenObjectSO
             });
-            
+
             return true;
         }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
     }
 }
