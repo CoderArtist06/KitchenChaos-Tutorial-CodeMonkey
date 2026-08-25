57ddd7f8d23247abd5688e826010408b85fbdae8
Valentin <cristian.valentin.ghita06@gmail.com>
2026-08-25 15:39:14 +0200

    Polish 6

--- Assets/Scripts/UI/DeliveryResultUI.cs
+++ Assets/Scripts/UI/DeliveryResultUI.cs
@@ -1,10 +1,53 @@
+using System;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DeliveryResultUI : MonoBehaviour
 {
+    private const string POPUP = "Popup";
+
     [SerializeField] private Image backgroundImage;
     [SerializeField] private Image iconImage;
     [SerializeField] private TextMeshProUGUI messegeText;
+    [SerializeField] private Color successColor;
+    [SerializeField] private Color failedColor;
+    [SerializeField] private Sprite successSprite;
+    [SerializeField] private Sprite failedSprite;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+
+        gameObject.SetActive(false);
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e)
+    {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
+
+        backgroundImage.color = successColor;
+        iconImage.sprite = successSprite;
+        messegeText.text = "DELIVERY\nSUCCESS";
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e)
+    {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
+
+        backgroundImage.color = failedColor;
+        iconImage.sprite = failedSprite;
+        messegeText.text = "DELIVERY\nFAILED";
+    }
 }
